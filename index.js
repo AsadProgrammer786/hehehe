32f968136758c362d4cb71c06294c7cff288a166
AsadProgrammer786 <102869276+AsadProgrammer786@users.noreply.github.com>
2022-04-11 15:30:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const app = express();
-const port = process.env.PORT || 80;
-app.listen(port, () => {
-    console.log("hi");
-});
-app.get("/", (req, res) => {
-res.end("kokoo");});
+// const fs = require("fs");
+// const app = express();
+// const port = process.env.PORT || 80;
+// app.listen(port, () => {
+//     console.log("hi");
+// });
+// app.get("/", (req, res) => {
+// res.end("kokoo");});
